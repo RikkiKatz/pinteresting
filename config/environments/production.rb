78955a6b7f3784568e8e9976d6b7caab5753e871
Rikki Katz <katz.rikki@gmail.com>
2014-03-19 14:51:52 -0400

    installed devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,11 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Required for Heroku
+  # Set to actual host
+  config.action_mailer.default_url_options = { host: 'sample-pinteresting.herokuapp.com' }
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Pinteresting::Application.routes.draw do
   root "pages#home"
-  get "about" => "pages#about" #creates about_path
+  get "about" => "pages#about" #creates about_path, defines root_url
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
