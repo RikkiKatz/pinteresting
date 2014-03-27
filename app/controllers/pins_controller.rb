5aed8a90ec61c0d26caceb8501da756a6bf19093
Rikki Katz <katz.rikki@gmail.com>
2014-03-27 17:00:42 -0400

    Added jQuery masonry

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,8 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:show, :edit, :destroy]
+  before_action :correct_user, only: [:edit, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @pins = Pin.all
   end
@@ -39,6 +40,7 @@ class PinsController < ApplicationController
   end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
