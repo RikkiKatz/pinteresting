8cfe315ba7c21d1efa9f5bbbf235f052f274df5b
Rikki Katz <katz.rikki@gmail.com>
2014-04-07 13:45:37 -0400

    Delete pins from users who delete their accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
-   has_many :pins
+   has_many :pins, depndent: :destroy
 
 	validates :name, presence: true
 end
